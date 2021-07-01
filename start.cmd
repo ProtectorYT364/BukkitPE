57c4f89158e03cead656e0d561d9691625f9b2ee
ProtectorYT364 <86392268+ProtectorYT364@users.noreply.github.com>
2021-07-01 20:55:51 +0530

    Update start.cmd

--- start.cmd
+++ start.cmd
@@ -3,5 +3,5 @@ title BukkitPE
 echo This is licensed under GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007.
 echo See here for more details.https://github.com/BukkitPE/BukkitPE/blob/master/LICENSE
 echo Starting BukkitPE
-java -jar BukkitPE.jar
+java -jar BukkitPE-1.jar
 pause
